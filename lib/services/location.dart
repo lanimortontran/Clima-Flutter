3004319605ace1c59d63962525d5c35b8a8e0283
Lani <lani.mortontran@movemedical.com>
2022-03-28 17:09:39 -0700

    144. Async/Await Challenge

---
+++ lib/services/location.dart
@@ -0,0 +1,44 @@
+import 'package:geolocator/geolocator.dart';
+
+class Location {
+  double latitude;
+  double longitude;
+
+  Future<void> getCurrentLocation() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    // Check if location services are enabled
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      return Future.error('Location services are disabled');
+    } else {
+      print('location services are enabled');
+    }
+
+    permission = await Geolocator.checkPermission();
+    print(permission);
+    if (permission == LocationPermission.denied) {
+      // Request permission if initially denied
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        return Future.error('Location permissions are denied');
+      }
+    }
+    if (permission == LocationPermission.deniedForever) {
+      return Future.error('Location permissions are permanently denied, we cannot request permissions.');
+    }
+
+    try {
+      print('getting position...');
+      Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.low,
+      );
+      print('position: $position');
+      latitude = position.latitude;
+      longitude = position.longitude;
+    } catch (e) {
+      print(e);
+    }
+  }
+}
